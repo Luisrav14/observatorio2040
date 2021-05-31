4972e546e8ec34a5aacf169defa0cd47e4b2c1d5
Luisrav14 <65048553+Luisrav14@users.noreply.github.com>
2021-05-31 10:21:37 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// Cambio de color scroll
+
+window.addEventListener("scroll", function () {
+  let header = document.querySelector("nav");
+  let windowPosition = window.scrollY > 0;
+  header.classList.toggle("scrolling-active", windowPosition);
+});
+
+// Graficas chart.js
+
+// Carrusel Publicaciones
+
+$(document).ready(function () {
+  $("#news-slider").owlCarousel({
+    items: 3,
+    navigation: true,
+    navigationText: ["", ""],
+    autoPlay: true,
+  });
+});
